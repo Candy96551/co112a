4cf3ecc00893629327d67e88f87d088ef6da880d
candy96551 <sean0206w@gmail.com>
2024-01-12 23:48:58 +0800

    all data

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -3,68 +3,55 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input.
+// Runs an infinite loop that listens to the keyboard input. 
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+// i.e. writes "black" in every pixel. When no key is pressed, the
+// program clears the screen, i.e. writes "white" in every pixel.
 
-(FOREVER)
-    @KBD     // 檢查鍵盤是否被按下
-    D=M      // D = *KBD
-    @NO_KEY_PRESSED
-    D;JEQ   // 如果 *KDB==0，跳轉到 NO_KEY_PRESSED
-
-    // 鍵盤被按下，將整個屏幕設為黑色
-    @SCREEN
-    D=A
-    @R0
-    M=D        // 將屏幕地址存入 R0
-
-    @4096      // 設置迴圈計數器（螢幕大小）
-    D=A
-    @BLACK_LOOP
-    M=D
-
-(BLACK_LOOP)
-    @R0    // 取得屏幕地址
-    D=M    // 使用 D 寄存器中的屏幕地址作為目標地址
-    A=D    // 使用 A 寄存器中的屏幕地址作為目標地址
-    M=-1   // 設置像素為黑色
-    @BLACK_LOOP
-    M=M-1  // 迭代次数减1
-    D=M
-    @BLACK_LOOP
-    D;JGT  // 如果还有迭代次数，继续循环
-
-    @FOREVER  // 回到 FOREVER，持續檢查鍵盤狀態
+    @status
+    M=-1        // status=0xFFFF
+    D=0         // Argument - what to set screen bits to
+    @SETSCREEN
     0;JMP
 
-(NO_KEY_PRESSED)
-    // 沒有按下鍵盤，將整個屏幕設為白色
-    @SCREEN
-    D=A
-    @R0
-    M=D        // 將屏幕地址存入 R0
-
-    @8192      // 設置迴圈計數器（螢幕大小）
-    D=A
-    @WHITE_LOOP
-    M=D
-
-(WHITE_LOOP)
-    @R0    // 取得屏幕地址
-    D=M    // 使用 D 寄存器中的屏幕地址作為目標地址
-    A=D    // 使用 A 寄存器中的屏幕地址作為目標地址
-    M=0    // 設置像素為白色
-    @WHITE_LOOP
-    M=M-1  // 迭代次数减1
+(LOOP)
+    @KBD
+    D=M         // D = current keyboard character
+    @SETSCREEN
+    D;JEQ       // If no key, set screen to zeroes (white)
+    D=-1        // If key pressed, set screen to all 1 bits (black)
+    
+(SETSCREEN)     // Set D=new status before jumping here
+    @ARG
+    M=D         // Save new status arg
+    @status     // FFFF=black, 0=white - status of entire screen
+    D=D-M       // D=newstatus-status
+    @LOOP
+    D;JEQ        // Do nothing if new status == old status
+    
+    @ARG
     D=M
-    @WHITE_LOOP
-    D;JGT  // 如果还有迭代次数，继续循环
-
-    @FOREVER  // 回到 FOREVER，持續檢查鍵盤狀態
+    @status
+    M=D         // status = ARG
+    
+    @SCREEN
+    D=A         // D=Screen address
+    @8192
+    D=D+A       // D=Byte just past last screen address
+    @i
+    M=D         // i=SCREEN address
+    
+(SETLOOP)    
+    @i
+    D=M-1
+    M=D         // i=i-1
+    @LOOP
+    D;JLT       // if i<0 goto LOOP
+    
+    @status
+    D=M         // D=status
+    @i
+    A=M         // Indirect
+    M=D         // M[current screen address]=status
+    @SETLOOP
     0;JMP
-
